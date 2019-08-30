7db7d362b17b670dc377ec374d492227cc840fec
aquadzn <williamjcqs8@gmail.com>
2019-08-30 16:21:41 +0200

    Add conda.sh

--- conda.sh
+++ conda.sh
@@ -18,12 +18,10 @@ if [ $1 = conda ]; then
     echo "$NAME installed!"
 elif [ $1 = miniconda ]; then
     NAME="Anaconda3"
-    VS="2019.07"
-    OS="Linux-x86_64"
 
     sudo apt-get update
     mkdir ~/.conda
-    wget https://repo.anaconda.com/miniconda/$NAME-$VER-$OS.sh -O ~/miniconda.sh
+    wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh
     bash ~/miniconda.sh -b -p ~/$NAME
     sudo rm ~/miniconda.sh
     export PATH=~/$NAME/bin:$PATH
