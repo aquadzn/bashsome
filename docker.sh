3bd19c22f85a4af62f9db36fc5195e873b9cdc88
aquadzn <williamjcqs8@gmail.com>
2019-08-31 15:53:57 +0200

    docker.sh

--- docker.sh
+++ docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd $HOME
-sudo apt-get remove docker docker-engine docker.io containerd runc
+sudo apt-get remove -y docker docker-engine docker.io containerd runc
 sudo apt-get update -y
 sudo apt-get install -y \
     apt-transport-https \
@@ -13,8 +13,8 @@ sudo add-apt-repository \
     "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
     $(lsb_release -cs) \
     stable"
-sudo apt-get update
-sudo apt-get install docker-ce
+sudo apt-get update -y
+sudo apt-get install -y docker-ce
 sudo usermod -aG docker $USER
 clear \
     && echo "Docker successfully installed!"
