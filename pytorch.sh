fdfdd647bdde300745c985a03f60198af4d3d1e1
William Jacques <46140458+aquadzn@users.noreply.github.com>
2019-09-02 21:31:24 +0200

    Update pytorch.sh

--- pytorch.sh
+++ pytorch.sh
@@ -2,14 +2,11 @@
 
 NAME=$1
 VS=$2
-conda create -y -n ${NAME} python=3.7
-eval "$(conda shell.bash hook)"
-conda activate ${NAME}
+conda create -y -n $NAME python=3.7
 if [ $VS = cpu ]; then
-    conda install -y pytorch torchvision cpuonly -c pytorch
+    conda install -y -n $NAME pytorch torchvision cpuonly -c pytorch
 elif [ $VS = gpu ]; then
-    conda install -y pytorch torchvision cudatoolkit=10.0 -c pytorch
+    conda install -y  -n $NAME pytorch torchvision cudatoolkit=10.0 -c pytorch
 fi
 clear \
     && echo "You can now activate $NAME environment."
-cd -
