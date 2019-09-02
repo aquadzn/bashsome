f4d14f90b5eef5f17330d36e084ceb77a2f75060
William Jacques <46140458+aquadzn@users.noreply.github.com>
2019-09-02 21:32:37 +0200

    Update vim.sh

--- vim.sh
+++ vim.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-cd $HOME
 
 echo "Checking if VIM already installed..."
-sudo apt-get purge vim -y && sudo rm -rf .vim && sudo rm -rf .vim*
+sudo apt-get purge vim -y && sudo rm -rf ~/.vim && sudo rm -rf ~/.vim*
 sudo apt-get install vim -y
 
 echo "Installing and configuring VIM / Vundle..."
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-cp $OLDPWD/src/.vimrc $HOME
+cp ./src/.vimrc ~/
 vim +PluginInstall +qall
 clear
 echo "VIM is installed!"
