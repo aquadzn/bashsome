6f911973bc449e501c424cdd2bc923d06cdc4f08
aquadzn <williamjcqs8@gmail.com>
2019-09-02 21:04:55 +0200

    Test (acp :robot:)

--- all.sh
+++ all.sh
@@ -5,10 +5,7 @@ ENV=$2
 VS=$3
 
 cd $HOME
-bash vim.sh
-bash conda.sh $CONDA
-bash docker.sh
-bash pytorch.sh $ENV $VS
+bash vim.sh && bash conda.sh $CONDA && bash docker.sh && bash pytorch.sh $ENV $VS
 clear \
     && echo "All done!"
 
